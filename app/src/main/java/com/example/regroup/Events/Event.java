3fb2621463506d433867ebaaeb7acdbca7789710
Segal1101 <44002817+Segal011@users.noreply.github.com>
2019-03-05 22:01:18 +0200

    first commit

---
+++ app/src/main/java/com/example/regroup/Events/Event.java
@@ -0,0 +1,50 @@
+package com.example.regroup.Events;
+
+import com.example.regroup.Users.user;
+
+import java.sql.Date;
+
+public class Event {
+
+
+    private String eventId;
+    private String name;
+    private Date date;
+    private user organizer;
+    public Event(String eventId, String name, Date date, user organizator){
+        this.eventId = eventId;
+        this.name = name;
+        this.date = date;
+        this.organizer = organizator;
+    }
+
+    public String getEventId(){
+        return eventId;
+    }
+    public void setEventId(String eventId){
+        this.eventId = eventId;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public Date getDate(){
+        return date;
+    }
+    public void setDate(Date date){
+        this.date = date;
+    }
+
+    public user getOrganizer(){
+        return organizer;
+    }
+    public void setOrganizaror(user organizator){
+        this.organizer = organizator;
+    }
+
+
+}
