350bd67f83a2ff4e242306bada3c15c34dce4ddf
TheWetBiscuit <neriuxas17@gmail.com>
2019-03-28 14:38:16 +0200

    added button onClick listener

--- app/src/main/java/com/example/regroup/LoginScreen.java
+++ app/src/main/java/com/example/regroup/LoginScreen.java
@@ -2,8 +2,15 @@ package com.example.regroup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginScreen extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +18,18 @@ public class LoginScreen extends AppCompatActivity {
         setContentView(R.layout.activity_login_screen);
 
         getSupportActionBar().hide(); // Hides the top action bar
+        mAuth = FirebaseAuth.getInstance(); // initializing FirebaseAuth instance
+
+        Button btn = findViewById(R.id.button3);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // When Login button is clicked, do something...
+                // Need to make it so when it's clicked it can't be clicked again until the credentials are checked and returned false
+
+                Toast.makeText(getApplicationContext(), "veikia", Toast.LENGTH_SHORT).show(); // Just for testing
+            }
+        });
     }
 }
