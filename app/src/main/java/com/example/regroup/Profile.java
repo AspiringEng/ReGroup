e0add4818671b422b426866db1ca0558df1eb427
Vytenis Kunickas <vytenis2008@gmail.com>
2019-04-17 13:12:00 +0300

    Fixed profile lag
    Put the image in drawable-nodpi folder

--- app/src/main/java/com/example/regroup/Profile.java
+++ app/src/main/java/com/example/regroup/Profile.java
@@ -1,5 +1,6 @@
 package com.example.regroup;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
