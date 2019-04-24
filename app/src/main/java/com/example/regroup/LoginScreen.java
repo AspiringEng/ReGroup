ef5a86dfcbd8f5de58aea297729323366c681f9c
TheWetBiscuit <neriuxas17@gmail.com>
2019-04-24 17:34:41 +0300

    Added facebook login
    Kažkiek veikia, bet nelabai išeina patestuoti, nes meta hash key klaidą. Gali būti dėl pačio FB Api.

--- app/src/main/java/com/example/regroup/LoginScreen.java
+++ app/src/main/java/com/example/regroup/LoginScreen.java
@@ -1,26 +1,41 @@
 package com.example.regroup;
 
-import android.content.Context;
 import android.content.Intent;
-import android.nfc.Tag;
-import android.provider.Settings;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class LoginScreen extends AppCompatActivity {
 
+    private CallbackManager mCallBackManager;
     private FirebaseAuth mAuth;
     private static final String TAG = MainActivity.class.getSimpleName();
     String uid;
@@ -39,10 +54,40 @@ public class LoginScreen extends AppCompatActivity {
             finish();
         }
 
+
         Button loginButton = findViewById(R.id.button3);
         Button registerButton = findViewById(R.id.button2);
 
+        // Initialize Facebook Login Button
+        //FacebookSdk.sdkInitialize(getApplicationContext());
+        mCallBackManager = CallbackManager.Factory.create();
+        LoginButton loginButtonFB = findViewById(R.id.fbsignup);
+        loginButtonFB.setReadPermissions("email", "public_profile");
 
+
+        // Facebook login button listener.
+        loginButtonFB.registerCallback(mCallBackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                Toast.makeText(getApplicationContext(), "FB login successful", Toast.LENGTH_SHORT).show();
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(), "Canceled", Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "facebook:onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "facebook:onError", error);
+            }
+        });
+
+        // Normal login button listener.
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -79,13 +124,48 @@ public class LoginScreen extends AppCompatActivity {
             }
         });
 
+        // Register Button listener.
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(LoginScreen.this, UserRegistration.class));
             }
         });
-
-
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result back to the Facebook SDK
+        mCallBackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                            startActivity(new Intent(LoginScreen.this, MainActivity.class));
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(LoginScreen.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
 }
