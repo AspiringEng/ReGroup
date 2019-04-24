1c34bcb4b14af7554ff22af9e538d970f98cca0d
mantasBrusokas <mantasbrusokas@gmail.com>
2019-04-24 14:13:14 +0300

    Uzregistravimas renginio

--- app/src/main/java/com/example/regroup/SettingsFragment.java
+++ app/src/main/java/com/example/regroup/SettingsFragment.java
@@ -9,13 +9,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingsFragment extends Fragment implements View.OnClickListener {
 
     private FirebaseAuth firebaseAuth;
-    private Button logout;
 
 
     @Nullable
@@ -23,15 +23,20 @@ public class SettingsFragment extends Fragment implements View.OnClickListener {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
         Button login = view.findViewById(R.id.loginba);
+        Button del = view.findViewById(R.id.delete);
         login.setOnClickListener(this);
         return view;
     }
+
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.loginba:
                 openLogin();
+                break;
+            case R.id.delete:
+                openDelete();
                 break;
         }
     }
@@ -42,4 +47,12 @@ public class SettingsFragment extends Fragment implements View.OnClickListener {
         Intent intent = new Intent(getActivity(), LoginScreen.class);
         startActivity(intent);
     }
+
+    public void openDelete(){
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.getCurrentUser().delete();
+        Intent intent = new Intent(getActivity(), LoginScreen.class);
+        startActivity(intent);
+    }
+
 }
